c003bab030f93262a5c9b3285e05305a794744e0
sonu <sonupandeybaldeo2003@gmail.com>
2023-12-16 18:25:33 +0530

    added htaccess

--- src/Components/ProjectSlider/Slider.jsx
+++ src/Components/ProjectSlider/Slider.jsx
@@ -22,7 +22,7 @@ const Slider = () => {
                     className="d-block w-100"
                     src="https://www.bhutanigroup.com/assets/images/slider/02c8afeff3f9b44750b92bf597a20338.jpg"
                     alt="Slide 1"
-                    style={{ maxHeight: '70vh', width: '100%' }} // Adjust the size of the image
+                    style={{ maxHeight: '800px', width: '100%' }} // Adjust the size of the image
                 />
             </Carousel.Item>
             <Carousel.Item className='CarouselItem'>
@@ -30,7 +30,7 @@ const Slider = () => {
                     className="d-block w-100 "
                     src="https://www.bhutanigroup.com/assets/images/slider/fb06429fbcdde7809c8a15e2da9ee6bd.webp"
                     alt="Slide 2"
-                    style={{ maxHeight: '70vh', width: '100%' }} // Adjust the size of the image
+                    style={{ maxHeight: '800px', width: '100%' }} // Adjust the size of the image
                 />
             </Carousel.Item>
             <Carousel.Item className='CarouselItem'>
@@ -38,7 +38,7 @@ const Slider = () => {
                     className="d-block w-100"
                     src="https://www.bhutanigroup.com/assets/images/slider/4a3bb113091b5036bed928ca17a7e011.jpg"
                     alt="Slide 3"
-                    style={{ maxHeight: '70vh', width: '100%' }} // Adjust the size of the image
+                    style={{ maxHeight: '800px', width: '100%' }} // Adjust the size of the image
                 />
             </Carousel.Item>
         </Carousel>
